55054eebebb3a90e9bf08badbe2fbc9e0fab6572
Curtis Vogt <curtis.vogt@gmail.com>
2025-04-08 14:40:06 -0500

    Skip unnamed projects

--- gen-pkg-src.jl
+++ gen-pkg-src.jl
@@ -3,19 +3,24 @@
 using Pkg
 
 env = Pkg.Types.EnvCache()
-project_dir = dirname(env.project_file)
+project_file = env.project_file
 project_name = env.project.name
-entry_file = joinpath(project_dir, "src", "$(project_name).jl")
 
-if !isfile(entry_file)
-    mkdir(dirname(entry_file))
+if project_name !== nothing
+    entry_file = joinpath(dirname(project_file), "src", "$(project_name).jl")
 
-    # Define an empty module to avoid warnings such as:
-    # ```
-    # WARNING: --output requested, but no modules defined during run
-    # ```
-    open(entry_file, "w") do io
-        println(io, "module $(project_name)")
-        println(io, "end")
+    if !isfile(entry_file)
+        mkdir(dirname(entry_file))
+
+        # Define an empty module to avoid warnings such as:
+        # ```
+        # WARNING: --output requested, but no modules defined during run
+        # ```
+        open(entry_file, "w") do io
+            println(io, "module $(project_name)")
+            println(io, "end")
+        end
     end
+else
+    @warn "Julia project is unnamed and does not require an entry source file"
 end
