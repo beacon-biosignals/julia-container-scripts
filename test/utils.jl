91724fb16dda173add0d365b244429d08d8923f4
Curtis Vogt <curtis.vogt@gmail.com>
2026-09-22 15:38:50 -0500

    fixup! Support for Julia 1.13

--- test/utils.jl
+++ test/utils.jl
@@ -120,7 +120,9 @@ function pkg_details(image::AbstractString, pkg::Base.PkgId)
         using Pkg: Pkg
         using UUIDs: UUID
         pkg = Base.PkgId($(pkg.uuid), $(pkg.name))
-        ji_path = if VERSION >= v"1.11"
+        ji_path = if VERSION >= v"1.13"
+            Base.compilecache_freshest_path(pkg)
+        elseif VERSION >= v"1.11"
             Base.compilecache_path(pkg)
         else
             paths = Base.find_all_in_cache_path(pkg)
