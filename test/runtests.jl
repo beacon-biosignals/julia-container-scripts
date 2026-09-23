06ef86088e070390b6589dcca7eeec6eb7b160e3
Curtis Vogt <curtis.vogt@gmail.com>
2026-09-23 10:02:03 -0500

    Add issue comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -112,6 +112,7 @@ include("utils.jl")
             @test "CompatLinearAlgebraExt" in basename.(dirname.(ji_files))
 
             pkg = PkgId(UUID("dbe5ba0b-aecc-598a-a867-79051b540f49"), "CompatLinearAlgebraExt")
+            # https://github.com/beacon-biosignals/julia-container-scripts/issues/15
             if VERSION >= v"1.13"
                 # Note: I'd prefer to use the value `missing` but that causes the tests to error
                 metadata = (; is_stdlib=false, in_sysimage=false, is_precompiled=false, ji_path="")
